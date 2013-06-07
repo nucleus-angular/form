29b543f9b1e5ab103c9ad9084bfbcedaef7e998f
Ryan Zec <code@ryanzec.com>
2013-06-07 17:58:56 -0400

    Code clean up

---
+++ module.js
@@ -0,0 +1,8 @@
+angular.module('nag.validate', [
+  'nag.validate.email',
+  'nag.validate.match',
+  'nag.validate.max',
+  'nag.validate.min',
+  'nag.validate.range',
+  'nag.validate.required'
+]);
